582f0b9628f1ba7baa3b1075a244182d9b427b7d
SanjayS808 <sanjaysugumar20@gmail.com>
2024-06-15 16:34:46 -0500

    added and tested requestqueue

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include "Request.h"
+#include "RequestQueue.h"
 
 int main() {
     Request test;
     Request test1;
     Request test2;
-    test.printRequest();
-    test1.printRequest();
-    test2.printRequest();
+
+    RequestQueue queue;
+    queue.addRequest(test);
+    queue.addRequest(test1);
+    queue.addRequest(test2);
+
+    queue.printRequestQueue();
+
+    cout << queue.isEmpty() << endl;
+    queue.getNextRequest();
+    queue.getNextRequest().printRequest();
+    queue.getNextRequest();
+    cout << queue.isEmpty() << endl;
+
 
 }
