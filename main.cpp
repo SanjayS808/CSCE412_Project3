60d53a83d2c070447e60758fdf495d3a02c89c12
SanjayS808 <sanjaysugumar20@gmail.com>
2024-06-15 16:08:22 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Request.h"
+
+int main() {
+    Request test;
+    Request test1;
+    Request test2;
+    test.printRequest();
+    test1.printRequest();
+    test2.printRequest();
+
+}
